4a3c45d0386275e37a60229efc54ffea6dfccb5f
justKvp <kvipka90@yandex.ru>
2024-09-19 01:14:30 +0300

    more @NotBlank

--- src/main/java/wowdb/io/pojo/requests/AccountCreateRq.java
+++ src/main/java/wowdb/io/pojo/requests/AccountCreateRq.java
@@ -1,22 +1,20 @@
 package wowdb.io.pojo.requests;
 
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import org.eclipse.microprofile.openapi.annotations.media.Schema;
 
 @Getter
 public class AccountCreateRq {
     @Schema(required = true, description = "Account username")
-    @NotNull(message = "account_name does not exist")
+    @NotBlank(message = "account_name does not exist")
     private String account_name;
 
-    @Min(message = "Minimal lenght of password : 1", value = 1)
     @Schema(required = true, description = "Account password")
-    @NotNull(message = "account_password does not exist")
+    @NotBlank(message = "account_password does not exist")
     private String account_password;
 
     @Schema(required = true, description = "Account email")
-    @NotNull(message = "account_email does not exist")
+    @NotBlank(message = "account_email does not exist")
     private String account_email;
 }
--- src/main/java/wowdb/io/pojo/requests/AccountVerifyRq.java
+++ src/main/java/wowdb/io/pojo/requests/AccountVerifyRq.java
@@ -1,5 +1,6 @@
 package wowdb.io.pojo.requests;
 
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import org.eclipse.microprofile.openapi.annotations.media.Schema;
@@ -7,10 +8,10 @@ import org.eclipse.microprofile.openapi.annotations.media.Schema;
 @Getter
 public class AccountVerifyRq {
     @Schema(required = true, description = "Account username")
-    @NotNull(message = "account_name does not exist")
+    @NotBlank(message = "account_name does not exist")
     private String account_name;
 
     @Schema(required = true, description = "Account password")
-    @NotNull(message = "account_password does not exist")
+    @NotBlank(message = "account_password does not exist")
     private String account_password;
 }
